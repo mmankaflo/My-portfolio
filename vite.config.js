422af9944d7ba2ca253a5f45b3f2f86db698ddc0
mmankaflo <florencemmanka02@gmail.com>
2025-07-02 17:26:48 +0200

    updated vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+/// <reference types="node" />
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    port: Number(process.env.PORT) || 5173,
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    port: Number(process.env.PORT) || 4173,
   },
 })
